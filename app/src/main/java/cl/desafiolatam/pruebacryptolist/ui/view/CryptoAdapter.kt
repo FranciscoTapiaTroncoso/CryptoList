4858e8f98f9d30609273dfe25e72066902167488
FranciscoTapiaTroncoso <franciscotapiatroncoso1@gmail.com>
2022-08-02 00:22:23 -0400

    Modificacion del adapter para contener multi viewholder

--- app/src/main/java/cl/desafiolatam/pruebacryptolist/ui/view/CryptoAdapter.kt
+++ app/src/main/java/cl/desafiolatam/pruebacryptolist/ui/view/CryptoAdapter.kt
@@ -1,48 +1,77 @@
 package cl.desafiolatam.pruebacryptolist.ui.view
 
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import cl.desafiolatam.pruebacryptolist.databinding.CryptoItemBinding
+import cl.desafiolatam.pruebacryptolist.databinding.CryptoItemNegativeBinding
 import cl.desafiolatam.pruebacryptolist.model.data.Image
 import cl.desafiolatam.pruebacryptolist.model.data.crypto.DataItem
+import cl.desafiolatam.pruebacryptolist.ui.view.Const.NEGATIVEVALUE
+import cl.desafiolatam.pruebacryptolist.ui.view.Const.POSITIVEVALUE
 import com.squareup.picasso.Picasso
 
-class CryptoAdapter(val clickListener: (crypto:DataItem)->Unit): RecyclerView.Adapter<CryptoAdapter.CryptoHolder>() {
+class CryptoAdapter(val clickListener: (crypto:DataItem)->Unit): RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
     private var cryptoList = mutableListOf<DataItem>()
+    private val image = Image()
 
-    class CryptoHolder(private val binding: CryptoItemBinding)
-        :RecyclerView.ViewHolder(binding.root) {
-        private val image = Image()
-            fun bind(crypto: DataItem, clickListener: (cryptoId:DataItem)->Unit){
-                binding.tvPriceUsd.text = "USD$ " + crypto.priceUsd
-                binding.tvCryptoSymbol.text = crypto.symbol
-                Picasso.get()
-                    .load(image.getImage(crypto))
-                    .resize(150,150)
-                    .centerCrop()
-                    .into(binding.ivCryptoLogo)
-                binding.root.setOnClickListener {
-                    clickListener(crypto)
-                }
-            }
 
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return if(viewType == POSITIVEVALUE){
+            val view = CryptoItemBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+            CryptoHolderWithPositivePriceChange(view)
+        }else{
+            val view = CryptoItemNegativeBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+            CryptoHolderWithNegativePriceChange(view)
+        }
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CryptoHolder {
-        val binding = CryptoItemBinding
-            .inflate(LayoutInflater.from(parent.context), parent, false)
-        return CryptoHolder(binding)
-    }
+//    override fun onBindViewHolder(holder: CryptoHolder, position: Int) {
+//        holder.bind(cryptoList[position], clickListener)
+//    }
 
-    override fun onBindViewHolder(holder: CryptoHolder, position: Int) {
-        holder.bind(cryptoList[position], clickListener)
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        return if(getItemViewType(position) == POSITIVEVALUE){
+            (holder as CryptoHolderWithPositivePriceChange).bind(cryptoList[position], clickListener)
+        }else{
+            (holder as CryptoHolderWithNegativePriceChange).bind(cryptoList[position],clickListener)
+        }
     }
 
     override fun getItemCount():Int{
         return cryptoList.size
+    }
+
+    inner class CryptoHolderWithPositivePriceChange(private val binding: CryptoItemBinding)
+        :RecyclerView.ViewHolder(binding.root) {
+        fun bind(crypto: DataItem, clickListener: (cryptoId:DataItem)->Unit){
+            binding.tvPriceUsd.text = "USD$ " + crypto.priceUsd
+            binding.tvCryptoSymbol.text = crypto.symbol
+            binding.tvPriceChange.text = crypto.changePercent24Hr
+            Picasso.get().load(image.getImage(crypto))
+                .resize(150,150)
+                .centerCrop().into(binding.ivCryptoLogo)
+            binding.root.setOnClickListener {
+                clickListener(crypto)
+            }
+        }
+    }
+
+    inner class CryptoHolderWithNegativePriceChange(private val binding: CryptoItemNegativeBinding)
+        :RecyclerView.ViewHolder(binding.root){
+        fun bind(crypto: DataItem, clickListener: (cryptoId:DataItem)->Unit){
+            binding.tvPriceUsdNegative.text = "USD$ " + crypto.priceUsd
+            binding.tvCryptoSymbolNegative.text = crypto.symbol
+            binding.tvPriceChangeNegative.text = crypto.changePercent24Hr
+            Picasso.get().load(image.getImage(crypto))
+                .resize(150,150)
+                .centerCrop().into(binding.ivCryptoLogoNegative)
+            binding.root.setOnClickListener {
+                clickListener(crypto)
+            }
+        }
     }
 
     fun update(list: List<DataItem>){
@@ -51,5 +80,17 @@ class CryptoAdapter(val clickListener: (crypto:DataItem)->Unit): RecyclerView.Ad
         notifyDataSetChanged()
     }
 
+    override fun getItemViewType(position: Int): Int {
+            if(cryptoList[position].changePercent24Hr.contentEquals("-")){
+                return NEGATIVEVALUE
+            } else{
+                return POSITIVEVALUE
+            }
+    }
 
+}
+
+private object Const{
+    const val POSITIVEVALUE = 0
+    const val NEGATIVEVALUE = 1
 }
